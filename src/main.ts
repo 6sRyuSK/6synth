13c6963d57c09ac09049753a04f61250d60e4175
6sRyuSK <zikkyou19@gmail.com>
2020-11-03 07:40:33 +0900

    modify main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import { Oscillator } from './oscillator'
 import './controllPanel'
 
 window.onload = () => {
-  const acceptAudio = window.confirm('再生を許可しますか？')
-  if(acceptAudio) {
-    window.sixSynth = new Oscillator()
-  }
+  window.sixSynth = new Oscillator()
 }
 
 window.addEventListener('DOMContentLoaded', () => {
