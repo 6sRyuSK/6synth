e876361a8900125c06e1146d8f6c844885a84b08
6sRyuSK <zikkyou19@gmail.com>
2020-11-03 07:41:01 +0900

    音声波形を実装

--- src/controllPanel.ts
+++ src/controllPanel.ts
@@ -3,3 +3,25 @@ const waveForm = document.getElementById('vco') as HTMLFormElement
 waveForm.onchange = () => {
   window.sixSynth.changeWave(waveForm.wave.value)
 }
+
+const waveform = document.getElementById('waveform') as HTMLCanvasElement
+const canvasContext = waveform.getContext('2d') as CanvasRenderingContext2D
+const intervalid = window.setInterval(function() {
+  // Get data for drawing sound wave
+  const times = new Uint8Array(window.sixSynth.analyser.fftSize);
+  window.sixSynth.analyser.getByteTimeDomainData(times);
+  // Clear previous data
+  canvasContext.clearRect(0, 0, waveform.width, waveform.height);
+  // Draw sound wave
+  canvasContext.beginPath();
+  for (var i = 0, len = times.length; i < len; i++) {
+      var x = (i / len) * waveform.width;
+      var y = (1 - (times[i] / 255)) * waveform.height;
+      if (i === 0) {
+          canvasContext.moveTo(x, y);
+      } else {
+          canvasContext.lineTo(x, y);
+      }
+  }
+  canvasContext.stroke();
+}, 1000 / 24);
--- src/oscillator.ts
+++ src/oscillator.ts
@@ -2,13 +2,17 @@ export class Oscillator {
   audioctx: AudioContext;
   osc!: OscillatorNode;
   waveType: waveType = 'sine'
+  analyser: AnalyserNode
   constructor() {
     this.audioctx = new AudioContext()
+    this.analyser = new AnalyserNode(this.audioctx)
+    this.analyser.connect(this.audioctx.destination)
+    this.analyser.fftSize = 128
   }
 
   play(frequency?: number) {
     this.osc = new OscillatorNode(this.audioctx)
-    this.osc.connect(this.audioctx.destination)
+    this.osc.connect(this.analyser)
     this.osc.type = this.waveType
     if(frequency) this.osc.frequency.setValueAtTime(frequency, this.audioctx.currentTime)
     // this.osc.detune.setValueAtTime(100, this.audioctx.currentTime)
