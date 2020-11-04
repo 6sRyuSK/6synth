574acc3850f76cebd1492d5976e1858df7cd9c22
6sRyuSK <zikkyou19@gmail.com>
2020-11-05 06:20:39 +0900

    add touchEvent

--- src/keyboard.ts
+++ src/keyboard.ts
@@ -7,6 +7,14 @@ const key = (keyArgs: KeyArgs) => {
   elm.className = 'key'
   elm.innerText = keyArgs.name
   elm.setAttribute('frequecy', keyArgs.frequency.toString())
+  elm.ontouchstart = (e) => {
+    e.preventDefault()
+    window.sixSynth.play(keyArgs.frequency)
+  }
+  elm.ontouchend = (e) => {
+    e.preventDefault()
+    window.sixSynth.stop()
+  }
   elm.onmousedown = () => window.sixSynth.play(keyArgs.frequency)
   elm.onmouseup = () => window.sixSynth.stop()
   return elm
